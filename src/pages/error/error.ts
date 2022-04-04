c810dde78bbf6bf65384be42844d9466ff1885bc
Aleksey Udalov <armiman25@yandex.ru>
2022-04-04 15:09:34 +0300

    Добавил страницу с ошибкой

---
+++ src/pages/error/error.ts
@@ -0,0 +1,15 @@
+import {Block} from '../../core';
+import './error.css';
+
+export class ErrorPage extends Block {
+  render() {
+    // language=hbs
+    return `
+      <section class="section error-page">
+        <h1 class="error-page__title">404</h1>
+        <p class="error-page__message">Не туда попали</p>
+        <a class="error-page__link" href="#">Назад к чатам</a>
+      </section>
+    `;
+  }
+}
